04cd087753fa6d9be84ddbcaf2271eb9b8613b29
Paulina Gryta <gryta.paulina@gmail.com>
2021-01-22 10:53:07 +0100

    HelloWord

---
+++ kodilla-intro/src/main/java/HelloWord.java
@@ -0,0 +1,10 @@
+public class HelloWord {
+    public static void main(String[] args) {         // [2]
+
+    System.out.println("Hello Word!");
+    System.out.println("Paulina");
+    System.out.println(12);
+    System.out.println('G');
+}
+
+}
