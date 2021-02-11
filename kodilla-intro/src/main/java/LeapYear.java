bf239694d231e76d9e43aa02c1ff004a6044e038
Paulina Gryta <gryta.paulina@gmail.com>
2021-02-11 12:44:40 +0100

    nowe klasy plus nowe metody

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,39 +1,50 @@
 public class LeapYear {
-
+    /**
+     * aaaaa
+     */
     public static void main(String[] args) {
-        int year = 2021;
+        int rok = 20;
         int a = 3;
         int b = 4;
         int c = 100;
-        int b = 400;
+        int d = 400;
+/*
+  dlaczego
+ */
 
-        if (przezCztery(2020,4) == 0) {
-            System.out.println("Przestepny");
-        } else{
-            System.out.println("nie przestepnyr");
+        if ((przezCztery(2020, 4) == 0)) {
+
+            if ((przezSto(4005, 100) == 0)) {
+
+                if ((przez400(400, 400) == 0)) {
+
+                    System.out.println("Przestepny");
+                }
+            } else {
+                System.out.println("nie przestepny");
+            }
         }
     }
 
-        private static int przezCztery(int rok, int liczba){
-            int result = rok % liczba;
-            System.out.println(result);
-            return result;
-        }
+    private static int przezCztery(int rok, int liczba) {
+        int result = rok % liczba;
+        System.out.println(result);
+        return result;
+    }
 
 
+    private static int przezSto(int rok, int liczba) {
+        int result = rok % liczba;
+        System.out.println(result);
+        return result;
+    }
 
 
-        private static void przez100(int year, int b){
-            int result = year % b;
-            System.out.println(result);
-        }
-        private static void przez400(int year, int c){
-            int result = year % c;
-            System.out.println(result);
-        }
-        private static void przez3(int year, int d){
-            int result = year % d;
-            System.out.println(result);
-        }
+    private static int przez400(int rok, int liczba) {
+        int result = rok % liczba;
+        System.out.println(result);
+        return result;
+    }
+
 }
 
