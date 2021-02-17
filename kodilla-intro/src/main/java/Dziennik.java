6a7aa8e3245f120206ad22315f49919b07617c94
Paulina Gryta <gryta.paulina@gmail.com>
2021-02-17 21:26:39 +0100

    Grades

---
+++ kodilla-intro/src/main/java/Dziennik.java
@@ -0,0 +1,26 @@
+public class Dziennik {
+    public static void main(String[] args) {
+        Grades oceny = new Grades();  // obiekt na podstawie klasy grades
+
+        // te oceny zostaya dodana do tablicy grades w obiekcie typu grades
+        // to jest miejsce do wpisywania ocen z ktorymi beda robione dzialania w klasie Grades
+        // jezeli wpisze wiecej ocen a mamy limit tablicy to nadmiarowe oceny nie zostana dodane do tablicy
+        oceny.add(5);
+        oceny.add(4);
+        oceny.add(3);
+        oceny.add(3);
+
+
+
+// nie wiem dla czego zawsze otrzymuje wynik 0
+        int ostatnia = oceny.ostatniaOcena();
+        System.out.println(ostatnia);
+
+// ta wartość nie jest wymagana w zadaniu
+        int suma = oceny.sumaWGrades();
+        System.out.println(suma);
+
+        int srednia = oceny.sredniaWGrades();
+        System.out.println(srednia);
+    }
+}
---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,48 @@
+public class Grades {
+    // to są zmienne w klasie grades
+    private int[] grades;   // ptivate czyli dostepne tylko w tej klasie
+    private int size;
+    private int suma;
+    private int ocena;
+    private int srednia;
+
+    public Grades() {   //to jest konstruktor klasy grades
+        this.grades = new int[10];
+        this.size = 0;
+        this.suma = 0;
+        this.ocena = 0;
+        this.srednia = 0;
+
+    }
+
+
+    public void add(int value) {  // to value jest to wartosc tymczasowa
+        if (this.size == 10) {  // zawsze musimy dodac ten warunek by nie przekroczyc wielkosci tablicy
+            return;
+        }
+        this.grades[this.size] = value; //  this.grades[1]   -  to jest dodanie oceny do tablicy pod okreslonym indeksem
+        this.size++;
+
+    }
+
+
+    public int ostatniaOcena() {
+        return this.grades[this.size];
+    }
+
+    public int sumaWGrades() {
+        for (int i = 0; i <= 9; i++) {
+            this.suma += grades[i];
+        }
+        return this.suma;
+
+    }
+
+    // saume za pomoca petli for i ja zwrocic
+    public int sredniaWGrades() {
+        for (int i = 0; i <= 9; i++) {
+            this.srednia = this.suma / this.size;
+        }
+        return this.srednia;
+    }
+}
